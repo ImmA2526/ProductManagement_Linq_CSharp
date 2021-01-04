4c2784197cf8918f8a444fbcb8939d4f1be4b299
Imran2526 <imraninfo97@gmail.com>
2021-01-04 15:01:00 +0530

    [Imran] Add :Welcome to Product MAnagenebt

---
+++ ProductReview/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviews
+{
+    class Program
+    {
+        /// <summary>
+        /// UC 1 Create List of Product Review
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        	Console.WriteLine("****************Welcome To Product MGt Using Linq*******");
+
+            List<ProductReview> list = new List<ProductReview>()
+            {
+            new ProductReview() {ProductId=1,UserId=1,Rating=5,Review="Good",isLike=true},
+            new ProductReview() { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 2, UserId = 1, Rating = 5, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 3, UserId = 1, Rating = 5, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 4, UserId = 1, Rating = 5, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 5, UserId = 1, Rating = 5, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 6, UserId = 1, Rating = 5, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 7, UserId = 1, Rating = 5, Review = "Good", isLike=false},
+            new ProductReview() { ProductId = 8, UserId = 1, Rating = 1.5, Review = "Good", isLike=false },
+            new ProductReview() { ProductId = 9, UserId = 1, Rating = 2.5, Review = "Bad", isLike=false },
+            new ProductReview() { ProductId = 10, UserId = 1, Rating = 5, Review = "Bad", isLike=false },
+            new ProductReview() { ProductId = 11, UserId = 1, Rating = 5, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 12, UserId = 1, Rating = 5, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 13, UserId = 1, Rating = 5, Review = "Bad", isLike=true },
+            new ProductReview() { ProductId = 14, UserId = 1, Rating = 5, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 15, UserId = 1, Rating = 5, Review = "Good", isLike=true }
+            };
+            foreach (var lists in list)
+            {
+                Console.WriteLine("Product id = " + lists.ProductId + "User id = " + lists.UserId + "Rating is = " + lists.Rating + " Review is = " + lists.Review + " isLike = " + lists.isLike);
+            }
+        }
+    }
+}
